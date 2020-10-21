2baed46c6477b7ec721f676e2e9f0901e8fdf821
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-10-21 17:58:13 +0300

    fix: inputs

--- momentum/assets/js/script.js
+++ momentum/assets/js/script.js
@@ -20,6 +20,8 @@ const weatherInput = document.getElementById('city')
 const weatherButton = document.querySelector('.weather__button')
 const weatherBlock = document.querySelector('.weather__container')
 
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
+
 if (localStorage.getItem('counter') === null) localStorage.setItem('counter', '0')
 if (localStorage.getItem('city') === null) localStorage.setItem('city', 'минск')
 
@@ -75,7 +77,6 @@ function showTime() {
 }
 
 function setBgGreet() {
-    const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg']
     if (+localStorage.getItem('counter') === images.length) localStorage.setItem('counter', '0')
     const today = new Date()
     const hour = today.getHours()
@@ -83,24 +84,28 @@ function setBgGreet() {
     const src = images[+localStorage.getItem('counter')]
 
     if (hour >= 6 && hour < 12) {
+        localStorage.setItem('folderCounter', '0')
         img.src = `./assets/img/morning/${src}`
         img.onload = () => {
             document.body.style.background = `center / cover no-repeat url('./assets/img/morning/${src}')`
         }
         greeting.textContent = 'Доброе утро,'
     } else if (hour >= 12 && hour < 18) {
+        localStorage.setItem('folderCounter', '1')
         img.src = `./assets/img/day/${src}`
         img.onload = () => {
             document.body.style.background = `center / cover no-repeat url('./assets/img/day/${src}')`
         }
         greeting.textContent = 'Добрый день,'
     } else if (hour >= 18 && hour < 24) {
+        localStorage.setItem('folderCounter', '2')
         img.src = `./assets/img/evening/${src}`
         img.onload = () => {
             document.body.style.background = `center / cover no-repeat url('./assets/img/evening/${src}')`
         }
         greeting.textContent = 'Добрый вечер,'
     } else {
+        localStorage.setItem('folderCounter', '3')
         img.src = `./assets/img/night/${src}`
         img.onload = () => {
             document.body.style.background = `center / cover no-repeat url('./assets/img/night/${src}')`
@@ -116,16 +121,40 @@ function nextImg() {
     setTimeout(() => nextImageButton.disabled = false, 1000)
 }
 
+function nextImgNew() {
+    localStorage.setItem('counter', `${+localStorage.getItem('counter') + 1}`)
+    if (+localStorage.getItem('counter') === images.length) {
+        localStorage.setItem('counter', '0')
+        localStorage.setItem('folderCounter', `${+localStorage.getItem('folderCounter') + 1}`)
+    }
+    const img = document.createElement('img')
+    const imageNumber = +localStorage.getItem('counter')
+    const folderNumber = +localStorage.getItem('folderCounter')
+
+
+    const folders = {
+        0: 'morning',
+        1: 'day',
+        2: 'evening',
+        3: 'night'
+    }
+
+    img.src = `./assets/img/night/${images[imageNumber]}`
+    img.onload = () => {
+        document.body.style.background = `center / cover no-repeat url('./assets/img/${folders[folderNumber]}/${images[imageNumber]}')`
+    }
+
+    // localStorage.setItem('folderCounter', `${+localStorage.getItem('folderCounter') + 1}`)
+}
+
 function getData(item, displayValue, editValue) {
     let localData = localStorage.getItem(item)
     if (!localData) {
         displayValue.textContent = ''
-        editValue.value = ''
         displayValue.style.display = 'none'
         editValue.style.display = 'block'
     } else {
         displayValue.textContent = localData
-        editValue.value = localData
         displayValue.style.display = 'block'
         editValue.style.display = 'none'
     }
@@ -134,12 +163,12 @@ function getData(item, displayValue, editValue) {
 function setName(e) {
     if (e.type === 'keypress') {
         if (e.code === 'Enter') {
-            localStorage.setItem('name', e.target.value.trim())
+            e.target.value.trim() && localStorage.setItem('name', e.target.value)
             name.blur()
             getData('name', displayName, name)
         }
     } else {
-        localStorage.setItem('name', e.target.value.trim())
+        e.target.value.trim() && localStorage.setItem('name', e.target.value)
         getData('name', displayName, name)
     }
 }
@@ -147,12 +176,12 @@ function setName(e) {
 function setFocus(e) {
     if (e.type === 'keypress') {
         if (e.code === 'Enter') {
-            localStorage.setItem('focus', e.target.value.trim())
+            e.target.value.trim() && localStorage.setItem('focus', e.target.value)
             focus.blur()
             getData('focus', displayFocus, focus)
         }
     } else {
-        localStorage.setItem('focus', e.target.value.trim())
+        e.target.value.trim() && localStorage.setItem('focus', e.target.value)
         getData('focus', displayFocus, focus)
     }
 }
@@ -160,12 +189,14 @@ function setFocus(e) {
 function editModeName() {
     displayName.style.display = 'none'
     name.style.display = 'block'
+    name.value = ''
     name.focus()
 }
 
 function editModeFocus() {
     displayFocus.style.display = 'none'
     focus.style.display = 'block'
+    focus.value = ''
     focus.focus()
 }
 
